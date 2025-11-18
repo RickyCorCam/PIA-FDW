da08d7d362173127c8b2633a8660e5d7fbebd7ba
Ricardo Cortes <ricardo.corcam@gmail.com>
2025-11-17 20:20:56 -0600

    Solucione arror runserver

--- Cafeteria/Core/urls.py
+++ Cafeteria/Core/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
 from . import views
 
-urlspatterns = [
-    path('', views.index, name='index'),
-    path('menu/', views.menu, name='menu'),
-    path('sucursales/', views.sucursales, name='sucursales'),
-    path('promociones/', views.promociones, name='promociones'),
-    path('comentarios/', views.comentarios, name='comentarios'),
-    path('comentarios/nuevo/', views.guardar_comentario, name='guardar_comentario'),
+urlpatterns = [
+    path('', views.index, name='index')
 ]
--- Cafeteria/Core/views.py
+++ Cafeteria/Core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'templates/Core/index.html')
+    return render(request, 'Core/index.html')
