267481bf6fdbebdb025066e9f39178e3aecd2c90
Ricardo Cortes <ricardo.corcam@gmail.com>
2025-11-17 20:01:55 -0600

    solucionando errores

--- Cafeteria/Core/urls.py
+++ Cafeteria/Core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlspatterns = [
-    path('', views.inicio, name='index'),
+    path('', views.index, name='index'),
     path('menu/', views.menu, name='menu'),
     path('sucursales/', views.sucursales, name='sucursales'),
     path('promociones/', views.promociones, name='promociones'),
