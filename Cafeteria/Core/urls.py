9cc10df8dc36918e009539dd76eab3b191b13dee
emilianoo8 <emilianoaleman1999@gmail.com>
2025-11-17 20:26:22 -0600

    Agrego vistas

--- Cafeteria/Core/urls.py
+++ Cafeteria/Core/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', views.menu, name='menu'),
+    path('sucursales/', views.sucursales, name='sucursales'),
+    path('promociones/', views.promociones, name='promociones'),
+    path('comentarios/', views.comentarios, name='comentarios'),
+    path('comentarios/nuevo/', views.guardar_comentario, name='guardar_comentario'),
 ]
