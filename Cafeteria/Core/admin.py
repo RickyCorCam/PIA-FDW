a1c26362a7eb43725faa027cceb8360e7ab373a0
emilianoo8 <emilianoaleman1999@gmail.com>
2025-11-17 21:31:49 -0600

    Agrego modelos

--- Cafeteria/Core/admin.py
+++ Cafeteria/Core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Producto, Sucursal, Promociones, Comentario
 
 # Register your models here.
+admin.site.register(Producto)
+admin.site.register(Sucursal)
+admin.site.register(Promociones)
+admin.site.register(Comentario)
