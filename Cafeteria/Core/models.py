c94d28ebd088d0fa559be0b0cedc31fd41ba0738
Ricardo Cortes <ricardo.corcam@gmail.com>
2025-11-17 21:27:24 -0600

    Agrego modelos

--- Cafeteria/Core/models.py
+++ Cafeteria/Core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Sucursal(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha = models.DateField()
+
+    def __str__(self):
+        return self.titulo
+
+
+class Comentario(models.Model):
+    nombre = models.CharField(max_length=100)
+    comentario = models.TextField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Promociones(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    descuento = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.titulo
