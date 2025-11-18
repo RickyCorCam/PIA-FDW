451606c555dfee18104391cea5ee074352d9df7d
Ricardo Cortes <ricardo.corcam@gmail.com>
2025-11-17 22:56:03 -0600

    Agrego evento

--- Cafeteria/Core/admin.py
+++ Cafeteria/Core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Producto, Sucursal, Promociones, Comentario
+from .models import Producto, Sucursal, Promociones, Comentario, Evento
 
 # Register your models here.
 admin.site.register(Producto)
 admin.site.register(Sucursal)
 admin.site.register(Promociones)
 admin.site.register(Comentario)
+admin.site.register(Evento)
