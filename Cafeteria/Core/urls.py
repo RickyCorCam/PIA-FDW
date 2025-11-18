b8d9278b52338c9541f40209036c2e3caf81784e
emilianoo8 <emilianoaleman1999@gmail.com>
2025-11-17 23:08:18 -0600

    Agrego

--- Cafeteria/Core/urls.py
+++ Cafeteria/Core/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('promociones/', views.promociones, name='promociones'),
     path('comentarios/', views.comentarios, name='comentarios'),
     path('comentarios/nuevo/', views.guardar_comentario, name='guardar_comentario'),
-    path("evento/", views.evento, name="evento"),
+    path("Evento/", views.Evento, name="Evento"),
 ]
