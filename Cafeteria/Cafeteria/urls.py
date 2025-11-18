5f99a8fe5c30b11059608788249fc86ac2d72fd3
Ricardo Cortes <ricardo.corcam@gmail.com>
2025-11-17 20:40:57 -0600

    cambios

--- Cafeteria/Cafeteria/urls.py
+++ Cafeteria/Cafeteria/urls.py
@@ -20,4 +20,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Core.urls')),
+    path('menu/', views.menu, name='menu'),
+    path('sucursales/', views.sucursales, name='sucursales'),
+    path('promociones/', views.promociones, name='promociones'),
+    path('comentarios/', views.comentarios, name='comentarios'),
+    path('comentarios/nuevo/', views.guardar_comentario, name='guardar_comentario'),
 ]
--- Cafeteria/Core/views.py
+++ Cafeteria/Core/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Producto, Sucursal, Promociones, Comentario
 
 # Create your views here.
 def index(request):
     return render(request, 'Core/index.html')
+def menu(request):
+    productos = Producto.objects.all()
+    return render(request, "menu.html", {"productos": productos})
+def sucursales(request):
+    lista = Sucursal.objects.all()
+    return render(request, "sucursales.html", {"sucursales": lista})
+def promociones(request):
+    promos = promociones.objects.all()
+    return render(request, "promociones.html", {"promos": promos})
+def comentarios(request):
+    lista = Comentario.objects.all()
+    return render(request, "comentarios.html", {"comentarios": lista})
+def guardar_comentario(request):
+    if request.method == "POST":
+        nombre = request.POST["nombre"]
+        texto = request.POST["comentario"]
+        Comentario.objects.create(nombre=nombre, comentario=texto)
+        return redirect("index")
